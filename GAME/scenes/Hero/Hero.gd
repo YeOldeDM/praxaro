3b58d7ac236a0018e208baf2ed3245939a6dcc2c
YeOldeDM <nines@email.com>
2017-10-18 01:46:50 -0600

    add InputMap controls, close #21

--- GAME/scenes/Hero/Hero.gd
+++ GAME/scenes/Hero/Hero.gd
@@ -1,10 +1,69 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+
+
+var facing = 1 setget _set_facing
+
+
+var velocity = Vector2()
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	### SETUP ###
+	var new_facing = self.facing
+	# Create force
+	var force = Vector2()
+	
+	
+	
+	### INPUT ###
+	
+	# Grab input
+	var LEFT = Input.is_action_pressed("LEFT")
+	var RIGHT = Input.is_action_pressed("RIGHT")
+	var UP = Input.is_action_pressed("UP")
+	var DOWN = Input.is_action_pressed("DOWN")
+	var JUMP = Input.is_action_pressed("JUMP")
+	var SHOOT = Input.is_action_pressed("STRIKE")
+	
+	
+	# Covert directional input into a Vector
+	var INPUT = Vector2( RIGHT - LEFT, DOWN - UP )
+	var vsign = sign(velocity.x)
+	var vlen = abs(velocity.x)
+	
+	
+	### FORCES ###
+	
+	# Define Aceleration/Deceleration to use
+	# If we can move and horizontal input is positive:
+		# Acelerate up to max speed or change direction
+	# otherwise decelerate toward zero speed
+	
+	# Jump
+	
+	# Kill upward vertical movement if JUMP control is let go
+	
+	
+	### DIRECT MOTION ###
+	
+	# Integrate force into velocity
+	# Turn velocity into motion
+	
+	
+	### COLLISION HANDLING ###
+	
+	
+	
+	### PREP NEXT FRAME ###
+	
+	
+	
+	
+	
+func _set_facing(what):
+	facing = what
+	if has_node("Sprite"):
+		get_node("Sprite").set_scale( Vector2( facing, 1 ) )
